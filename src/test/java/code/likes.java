ee653f2972dcb9b2e3779f0ddcbbbb1bab8083ff
PonmalarPoyyaliraj <pon788782@gmail.com>
2024-05-30 16:27:56 +0530

    message

---
+++ src/test/java/code/likes.java
@@ -0,0 +1,67 @@
+package code;
+
+
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class likes {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\balap\\Downloads\\edgedriver_win64");
+		
+        WebDriverManager.edgedriver().setup();
+		EdgeOptions option=new EdgeOptions();
+		option.addArguments("disable-notifications");
+		option.addArguments("disable-popups");
+		option.addArguments("start-maximized");
+		option.addArguments("--headless");
+		 WebDriver driver=new EdgeDriver(option);
+		driver.navigate().to("https://www.redbus.in/");
+		System.out.println("ponmalarraj");
+	}
+	}   
+	   
+
